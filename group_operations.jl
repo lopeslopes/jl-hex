d2781a5abb1bf69e2cce6ed10e8b4140a9b334b7
Mateus Lopes <mateuslmazziero@gmail.com>
2024-06-01 20:19:33 -0300

    WIP searching representations of aligned points

--- group_operations.jl
+++ group_operations.jl
@@ -20,8 +20,9 @@ latBB = read_lattice("data/0.0191434/"*name*"_BB.dat")
 treeAB = KDTree(transpose(latAB))
 ind, dist = knn(treeAB, origin, 1000)
 lenBB = size(latBB)[1]
-println(lenBB)
 
+point_AB = [0.0, 0.0]
+point_BB = [0.0, 0.0]
 slope_final = 0.0
 for i in 1:1000
     point = latAB[ind[i],:]
@@ -30,11 +31,17 @@ for i in 1:1000
         y_slope = latBB[j,1]*slope
         diff = abs(latBB[j,2]-y_slope)
         if diff<0.1
-            println(i, point)
-            println("    ", j, " ", diff)
-            point_final = point
-            global slope_final = point_final[2]/point_final[1]
-            println(slope_final)
+            println("AB point: ")
+            println("    index:  ", i)
+            println("    coords: ", point)
+            println("BB point: ")
+            println("    index:  ", j)
+            println("    coords: ", latBB[j,:])
+            println("    diff:   ", diff)
+            global point_AB = point
+            global point_BB = latBB[j,:]
+            global slope_final = point_AB[2]/point_AB[1]
+            println("Slope of line: ", slope_final)
         end
     end
 end
@@ -43,16 +50,20 @@ x_vals = -12500:12500
 y_vals = slope_final*x_vals
 
 ax1 = subplot(111, aspect=1)
-ax1.scatter(latAA[:,1], latAA[:,2], color="green", s=8)
-ax1.scatter(latBA[:,1], latBA[:,2], color="red", s=8)
-ax1.scatter(latAB[:,1], latAB[:,2], color="orange", s=8)
-ax1.scatter(latBB[:,1], latBB[:,2], color="blue", s=8)
+ax1.scatter(latAA[:,1], latAA[:,2], color=("green", 0.5), s=8)
+ax1.scatter(latBA[:,1], latBA[:,2], color=("red", 0.5), s=8)
+ax1.scatter(latAB[:,1], latAB[:,2], color=("orange", 0.5), s=8)
+ax1.scatter(latBB[:,1], latBB[:,2], color=("blue", 0.5), s=8)
 
-#ax1.quiver(0.0, 0.0, point[1], point[2], angles="xy", scale_units="xy", scale=1)
+# SELECTED AB and BB points
+ax1.scatter(0.0, 0.0, color="green", s=50)
+ax1.scatter(point_AB[1], point_AB[2], color="magenta", s=50)
+ax1.scatter(point_BB[1], point_BB[2], color="purple", s=50)
+
 ax1.plot(x_vals, y_vals)
 
 ax1.set_xlim([-12250, 12250])
 ax1.set_ylim([-12250, 12250])
-legend(["AA", "BA", "AB", "BB"])
+legend(["AA", "BA", "AB", "BB", "selected AA", "selected AB", "selected BB"])
 
 show()
