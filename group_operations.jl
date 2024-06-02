a5387f1327c51b0b83e52cb5894785624bc5b516
Mateus Lopes <mateuslmazziero@gmail.com>
2024-06-02 19:32:36 -0300

    WIP finding representation of points

--- group_operations.jl
+++ group_operations.jl
@@ -21,6 +21,7 @@ treeAB = KDTree(transpose(latAB))
 ind, dist = knn(treeAB, origin, 1000)
 lenBB = size(latBB)[1]
 
+# FINDING POINTS THAT ALIGN
 point_AB = [0.0, 0.0]
 point_BB = [0.0, 0.0]
 slope_final = 0.0
@@ -31,13 +32,11 @@ for i in 1:1000
         y_slope = latBB[j,1]*slope
         diff = abs(latBB[j,2]-y_slope)
         if diff<0.1
-            println("AB point: ")
-            println("    index:  ", i)
-            println("    coords: ", point)
-            println("BB point: ")
-            println("    index:  ", j)
-            println("    coords: ", latBB[j,:])
-            println("    diff:   ", diff)
+            # println("AB point: ", point)
+            # println("    index:  ", i)
+            # println("BB point: ", latBB[j,:])
+            # println("    index:  ", j)
+            println("    diff: ", diff)
             global point_AB = point
             global point_BB = latBB[j,:]
             global slope_final = point_AB[2]/point_AB[1]
@@ -46,22 +45,62 @@ for i in 1:1000
     end
 end
 
+# FIGURING OUT AB POINT COORDINATES IN RELATION TO LATTICE VECTORS A1 AND A2
+lat_angle = pi/3.0
+a = 2.46
+a1 = [a, 0.0]
+a2 = [a*cos(lat_angle), a*sin(lat_angle)]
+
+d = sqrt((a^2)/(2.0*(1.0-cos(2.0*lat_angle))))
+d1 = [d*cos(lat_angle/2.0), d*sin(lat_angle/2.0)]
+origin_a = [0.0, 0.0]
+origin_b = origin_a + d1
+
+qtd_a2_AB = round((point_AB[2] - origin_a[2])/a2[2])
+AB_aux = origin_a + qtd_a2_AB*a2
+qtd_a1_AB = round((point_AB[1] - AB_aux[1])/a1[1])
+println("AB point: ", point_AB)
+println("    Number of a1: ", qtd_a1_AB)
+println("    Number of a2: ", qtd_a2_AB)
+AB_test = origin_a + qtd_a1_AB*a1 + qtd_a2_AB*a2
+println("    Point obtained with lattice vectors: ", AB_test)
+dist_AB = sqrt(point_AB[1]^2 + point_AB[2]^2)
+println("    Distance from origin: ", dist_AB)
+
+# FIGURING OUT BB POINT COORDINATES IN RELATION TO LATTICE VECTORS A1 AND A2
+qtd_a2_BB = round((point_BB[2] - origin_b[2])/a2[2])
+BB_aux = origin_b + qtd_a2_BB*a2
+qtd_a1_BB = round((point_BB[1] - BB_aux[1])/a1[1])
+println("BB point: ", point_BB)
+println("    Number of a1: ", qtd_a1_BB)
+println("    Number of a2: ", qtd_a2_BB)
+BB_test = origin_b + qtd_a1_BB*a1 + qtd_a2_BB*a2
+println("    Point obtained with lattice vectors: ", BB_test)
+dist_BB = sqrt(point_BB[1]^2 + point_BB[2]^2)
+println("    Distance from origin: ", dist_BB)
+
+println("Ratio distAB/distBB: ", (dist_AB+dist_BB)/dist_BB)
+
+# LINE CROSSING AA, AB AND BB POINTS
 x_vals = -12500:12500
 y_vals = slope_final*x_vals
 
+# PLOTTING ALL AA, BA, AB AND BB POINTS
 ax1 = subplot(111, aspect=1)
 ax1.scatter(latAA[:,1], latAA[:,2], color=("green", 0.5), s=8)
 ax1.scatter(latBA[:,1], latBA[:,2], color=("red", 0.5), s=8)
 ax1.scatter(latAB[:,1], latAB[:,2], color=("orange", 0.5), s=8)
 ax1.scatter(latBB[:,1], latBB[:,2], color=("blue", 0.5), s=8)
 
-# SELECTED AB and BB points
+# SELECTED AB AND BB POINTS
 ax1.scatter(0.0, 0.0, color="green", s=50)
 ax1.scatter(point_AB[1], point_AB[2], color="magenta", s=50)
 ax1.scatter(point_BB[1], point_BB[2], color="purple", s=50)
 
+# PLOTTING LINE
 ax1.plot(x_vals, y_vals)
 
+# SETTING PLOT LIMITS AND LEGEND
 ax1.set_xlim([-12250, 12250])
 ax1.set_ylim([-12250, 12250])
 legend(["AA", "BA", "AB", "BB", "selected AA", "selected AB", "selected BB"])
