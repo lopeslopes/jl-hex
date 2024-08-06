7a8ead3646a8bf3eed78c98c29e289f024511bce
Mateus Lopes <mateuslmazziero@gmail.com>
2024-08-06 17:39:15 -0300

    WIP symmetry analysis

--- test.py
+++ test.py
@@ -28,11 +28,21 @@ d1 = [d * np.cos(np.pi / 6), d * np.sin(np.pi / 6), 0.0]
 #                     [a/2, a*np.sqrt(3)/2, 0.0],
 #                     [0.0, 0.0, 2000.0]])
 
+# lattice = np.array([[2931.090344051522, -36.2171866374553, 0.0],
+#                     [-1434.1801683441458, 2556.5072920550774, 0.0],
+#                     [0.0, 0.0, 50000.0]])
+
+lattice = np.array([[4428.000519758899, 2484.072918780167, 0.0],
+                    [62.730007363230285, 5076.797397472701, 0.0],
+                    [0.0, 0.0, 50000.0]])
+
 # DEFINE ATOMS IN UNIT CELL
+
 # atoms = [[0.0, 0.0, 0.0],
 #         [a/2, a*np.sqrt(3)/2, 0.0],
 #         [a, 0.0, 0.0],
 #         d1]
+
 atomsAA = read_lattice("data/0.0191435_0.001/latticeAA.dat")
 lenAA = len(atomsAA)
 numbersAA = np.ones(lenAA)
@@ -49,39 +59,49 @@ atomsBB = read_lattice("data/0.0191435_0.001/latticeBB.dat")
 lenBB = len(atomsBB)
 numbersBB = 4 * np.ones(lenBB)
 
-# DEFINE LATTICE VECTORS
-lattice = np.array([[4428.000519758899, 2484.072918780167, 0.0],
-                    [62.730007363230285, 5076.797397472701, 0.0],
-                    [0.0, 0.0, 50000.0]])
-
-
-#lattice = np.array([[2931.090344051522, -36.2171866374553, 0.0],
-#                    [-1434.1801683441458, 2556.5072920550774, 0.0],
-#                    [0.0, 0.0, 50000.0]])
-
 positions = np.concatenate((atomsAA, atomsAB))
 numbers = np.concatenate((numbersAA, numbersAB))
 
 graphene_structure = (lattice, positions, numbers)
 
+# SYMMETRY CALCULATIONS
 sym_data = spglib.get_symmetry_dataset(graphene_structure, symprec=1e-5)
 print(f"{sym_data.number=}")
 print(f"{sym_data.hall_number=}")
 print(f"{sym_data.international=}")
 print(f"{sym_data.hall=}")
-mesh = [10, 10, 10]
 
-# Gamma centre mesh
+# GAMMA CENTERED MESH
+mesh = [10, 10, 10]
 mapping, grid = spglib.get_ir_reciprocal_mesh(mesh, graphene_structure, is_shift=[0, 0, 0])
 # for i, (ir_gp_id, gp) in enumerate(zip(mapping, grid)):
 #    print("%3d ->%3d %s" % (i, ir_gp_id, gp.astype(float) / mesh))
 
-# Irreducible k-points
+# IRREDUCIBLE K-POINTS
 print("Number of ir-kpoints: %d" % len(np.unique(mapping)))
 unique_kpts = np.transpose(grid[np.unique(mapping)] / np.array(mesh, dtype=float))
-all_kpts = np.transpose(grid)
 
-ax1 = plt.subplot(111, projection="3d")
+atomsAA = np.array(atomsAA)
+atomsBA = np.array(atomsBA)
+atomsAB = np.array(atomsAB)
+atomsBB = np.array(atomsBB)
+# print(atomsAA)
+
+# PLOT OF K POINTS
+ax1 = plt.subplot(211, projection="3d")
 ax1.scatter(unique_kpts[0, :], unique_kpts[1, :], unique_kpts[2, :])
-# ax1.scatter(all_kpts[0,:], all_kpts[1,:], all_kpts[2,:])
+
+# PLOT OF DIRECT SPACE THINGS
+ax2 = plt.subplot(212)
+ax2.scatter(atomsAA[:,0], atomsAA[:,1], s=20, color="c")
+ax2.scatter(atomsBA[:,0], atomsBA[:,1], s=20, color="green")
+ax2.scatter(atomsAB[:,0], atomsAB[:,1], s=20, color="orange")
+ax2.scatter(atomsBB[:,0], atomsBB[:,1], s=20, color="red")
+
+ax2.quiver(0.0, 0.0, lattice[0,0], lattice[0,1], angles="xy", scale_units="xy", scale=1)
+ax2.quiver(0.0, 0.0, lattice[1,0], lattice[1,1], angles="xy", scale_units="xy", scale=1)
+
+ax1.set_aspect("equal")
+ax2.set_aspect("equal")
+
 plt.show()
