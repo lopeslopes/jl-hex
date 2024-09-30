a029810f02e8ebefc95d593047b503d19f12c1d5
Mateus Lopes <mateuslmazziero@gmail.com>
2024-09-30 11:44:21 -0300

    Testing ASE

--- test_magmom.jl
+++ test_magmom.jl
@@ -1,7 +1,7 @@
 include("hex_utils.jl")
 using .HexUtils
 using PyCall
-using PyCall: LinearAlgebra
+using LinearAlgebra
 pygui(:qt5)
 using PyPlot
 using NearestNeighbors
@@ -10,42 +10,62 @@ using Combinatorics
 using StatsBase
 
 spglib = pyimport("spglib")
+ase = pyimport("ase")
+sg = pyimport("ase.spacegroup")
+viz = pyimport("ase.visualize.plot")
 
-# lattice = [[4428.000519758899, 2484.072918780167, 0.0],
-#            [-4365.2705123956675, 2592.7244786925335, 0.0],
-#            [0.0, 0.0, 10000.0]]
+lattice = [[4428.000519758899, 2484.072918780167, 0.0],
+           [-4365.2705123956675, 2592.7244786925335, 0.0],
+           [0.0, 0.0, 10000.0]]
 
-lattice = [ [sqrt(3)/2, 1/2, 0.0],
-            [-sqrt(3)/2, 1/2, 0.0],
-            [0.0, 0.0, 10000]]
+# lattice = [ [sqrt(3)/2, 1/2, 0.0],
+#             [-sqrt(3)/2, 1/2, 0.0],
+#             [0.0, 0.0, 10000]]
 
-a1_norm = LinearAlgebra.norm(lattice[1, :])
-a2_norm = LinearAlgebra.norm(lattice[2, :])
-a3_norm = LinearAlgebra.norm(lattice[3, :])
+a1 = lattice[1,:][1]
+a2 = lattice[2,:][1]
+a3 = lattice[3,:][1]
 
-angle = pi/3
-d = sqrt((a1_norm^2)/(2.0*(1.0-cos(2.0*angle))))
-d1 = [d*cos(angle/2.0), d*sin(angle/2.0), 0.0]
+cell_vol = dot(a1, cross(a2, a3))
+b1 = 2.0*pi*cross(a2, a3)/cell_vol
+b2 = 2.0*pi*cross(a3, a1)/cell_vol
+b3 = 2.0*pi*cross(a1, a2)/cell_vol
 
-positions = [[0.0, 0.0, 0.0],
-             d1]
+positions = [[0.0, 0.0, 0.0]]
+numbers = [1]
+graphene = (lattice, positions, numbers)
+sym_data = spglib.get_symmetry_dataset(graphene)
 
-numbers = [1, 2]
-spins = [1.0, 1.0]
+mesh = [20, 20, 20]
+mapping, grid = spglib.get_ir_reciprocal_mesh(mesh, graphene, is_shift=[0, 0, 0])
 
-graphene = (lattice, positions, numbers, spins)
+un_kpoints = []
+un_ind = unique(mapping)
+for j in un_ind
+    i = j+1
+    kp = grid[i,1]*b1 .+ grid[i,2]*b2 .+ grid[i,3]*b3
+    push!(un_kpoints, kp)
+end
+un_kpoints = hcat(un_kpoints...)
+un_kpoints = transpose(un_kpoints)
 
-sym_data = spglib.get_magnetic_symmetry_dataset(graphene)
+ax1 = subplot(211, projection="3d")
+ax1.scatter(un_kpoints[:,1], un_kpoints[:,2], un_kpoints[:,3], color="c")
 
-println("Group number:           ", sym_data.uni_number)
-println("Hall number:            ", sym_data.hall_number)
-println("Symmetry operations:    ", sym_data.n_operations)
+# ASE FEATURES TESTING
+supercell = ase.cell.Cell.new(transpose(lattice))
+println(supercell.lengths())
 
-ax1 = subplot(111, projection="3d")
-ax1.scatter(lattice[1][1], lattice[1][2], lattice[1][3])
-ax1.scatter(lattice[2][1], lattice[2][2], lattice[2][3])
-ax1.scatter(lattice[3][1], lattice[3][2], lattice[3][3])
-ax1.scatter(d1[1], d1[2], d1[3])
-ax1.scatter(0.0, 0.0, 0.0)
+kcell = supercell.reciprocal()
+println(kcell.lengths())
 
+# ax2 = subplot(212)
+# graphene_test = sg.crystal("C", [(0,0,0)], spacegroup=183, cellpar=supercell.cellpar(), size=(5,5,5))
+# viz.plot_atoms(graphene_test, ax2)
+
+# path = supercell.bandpath("KGLK", 100)
+# test_path = path.cartesian_kpts()
+# ax1.scatter(test_path[:,1], test_path[:,2], test_path[:,3], color="red")
+
+ax1.set_aspect("equal")
 show()
