3d183febd29eb99bb2508b2685a2bd154fa6082a
Mateus Lopes <mateuslmazziero@gmail.com>
2025-08-27 17:49:59 -0300

    added 1 method of distortion, working on the second

--- distortion.jl
+++ distortion.jl
@@ -10,6 +10,10 @@ AA_sep = Float64[]
 BA_sep = Float64[]
 AB_sep = Float64[]
 BB_sep = Float64[]
+AA_vec = []
+BA_vec = []
+AB_vec = []
+BB_vec = []
 
 open("AAstack_separations", "r") do f
     for line in eachline(f)
@@ -27,20 +31,33 @@ open("AAstack_separations", "r") do f
         elseif startswith(line, "BB:")
             aux = split(line, ":")
             push!(BB_sep, parse(Float64, aux[2]))
+        elseif startswith(line, "AA_vec:")
+            aux = split(line, ":")
+            aux2 = split(strip(aux[2]), ",")
+            push!(AA_vec, [parse(Float64, lstrip(aux2[1], '[')), parse(Float64, aux2[2]), parse(Float64, rstrip(aux2[3], ']'))])
+        elseif startswith(line, "BA_vec:")
+            aux = split(line, ":")
+            aux2 = split(strip(aux[2]), ",")
+            push!(BA_vec, [parse(Float64, lstrip(aux2[1], '[')), parse(Float64, aux2[2]), parse(Float64, rstrip(aux2[3], ']'))])
+        elseif startswith(line, "AB_vec:")
+            aux = split(line, ":")
+            aux2 = split(strip(aux[2]), ",")
+            push!(AB_vec, [parse(Float64, lstrip(aux2[1], '[')), parse(Float64, aux2[2]), parse(Float64, rstrip(aux2[3], ']'))])
+        elseif startswith(line, "BB_vec:")
+            aux = split(line, ":")
+            aux2 = split(strip(aux[2]), ",")
+            push!(BB_vec, [parse(Float64, lstrip(aux2[1], '[')), parse(Float64, aux2[2]), parse(Float64, rstrip(aux2[3], ']'))])
         end
     end
 end
 
 min_index = argmin(AB_sep)
-println("Angle:      ", angles[min_index])
-println("Separation: ", AB_sep[min_index])
+println("Angle:       ", angles[min_index])
+println("Separation:  ", AB_sep[min_index])
+println("Sep. Vector: ", AB_vec[min_index])
 
 
-## TODO: generate distorted lattices A2 and B2, based on the found separation
-
-
-
-## finding AA, AB, BA, BB points for the angle and new A2, B2 lattices
+## reading files path for the selected angle, loading lattice1
 base_data_path = "data"
 entries = readdir(base_data_path; join=true)
 dataset_dirs = sort(filter(isdir, entries))
@@ -48,22 +65,37 @@ n_datasets = length(dataset_dirs)
 
 path = dataset_dirs[min_index]
 angle_name = path[findfirst("_",path)[1]+1:findlast("_", path)[1]-1]
-println(angle_name)
 
 latA1 = transpose(read_lattice_3d(path*"/latticeA1.dat"))
 latB1 = transpose(read_lattice_3d(path*"/latticeB1.dat"))
+
+
+## DISTORTION
+## Method 1: adding the separation vector directly to all points in the lattice
+## just a whole lattice dislocation, primitive cell remains with the same area
 latA2 = transpose(read_lattice_3d(path*"/latticeA2.dat"))
 latB2 = transpose(read_lattice_3d(path*"/latticeB2.dat"))
 
+latA2_distorted = latA2 .+ AB_vec[min_index]
+latB2_distorted = latB2 .+ AB_vec[min_index]
+
+## Method 2: changing the a1 and a2 vectors based on the separation vector
+## and then generating the whole lattice again using modified lat vectors
+## TODO: calculate area of primitive cell
+
+
+
+
+## finding AA, AB, BA, BB points for the angle and new A2, B2 lattices
 treeA1 = KDTree(latA1)
 treeB1 = KDTree(latB1)
 
 latA1 = transpose(latA1)
 latB1 = transpose(latB1)
-latA2 = transpose(latA2)
-latB2 = transpose(latB2)
+latA2 = transpose(latA2_distorted)
+latB2 = transpose(latB2_distorted)
 
-tol = 5.0e-3
+tol = 1.0e-3
 println("Tolerance:        ", tol)
 name = @sprintf("%6.4f", tol)
 
